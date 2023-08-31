04b6b32c9ce518b3027972050da6c5ebddfd8aa5
Oleh Protsenko <designofhumanity@gmail.com>
2023-08-31 21:02:13 +0300

    better structure

---
+++ relationship_calculator.py
@@ -0,0 +1,120 @@
+
+from itertools import permutations
+from typology_temporistics import TypologyTemporistics
+
+class RelationshipCalculator:
+    """A class to calculate the relationship type and comfort score between two users based on their typology aspects."""
+    
+
+
+    # Relationship types
+    RELATIONSHIP_PHILIA = "Philia"
+    RELATIONSHIP_PSEUDO_PHILIA = "Pseudo-Philia"
+    RELATIONSHIP_AGAPE = "Agape"
+    RELATIONSHIP_FULL_AGAPE = "Full Agape"
+    RELATIONSHIP_EROS = "Eros"
+    RELATIONSHIP_EROS_VARIANT = "Eros Variant"
+    RELATIONSHIP_FULL_EROS = "Full Eros"
+    
+    # Comfort scores for each relationship type
+    COMFORT_SCORES = {
+        RELATIONSHIP_PHILIA: 5,
+        RELATIONSHIP_PSEUDO_PHILIA: 3,
+        RELATIONSHIP_AGAPE: 8,
+        RELATIONSHIP_FULL_AGAPE: 10,
+        RELATIONSHIP_EROS: -2,
+        RELATIONSHIP_EROS_VARIANT: -1,
+        RELATIONSHIP_FULL_EROS: -5
+    }
+
+        # Terminal color codes
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
+    # ... [rest of your class definition remains unchanged]
+
+    def get_relationship_color(self, comfort_score):
+        """Return the color code based on comfort score."""
+        if comfort_score > 0:
+            return self.GREEN
+        elif comfort_score < 0:
+            return self.RED
+        else:
+            return self.YELLOW
+
+    def __init__(self, user1, user2, typology):
+        """Initialize the class with two users."""
+        self.user1 = user1
+        self.user2 = user2
+        self.typology = typology
+
+    def determine_relationship_type(self):
+        """Determine the type of relationship between the two users."""
+        if self.user1 == self.user2:
+            return self.RELATIONSHIP_PHILIA
+        
+        # Logic for Pseudo-Philia
+        if (
+            (self.user1[0] == self.user2[0] and self.user1[3] == self.user2[3]) or
+            (self.user1[1] == self.user2[1] and self.user1[2] == self.user2[2])
+        ):
+            return self.RELATIONSHIP_PSEUDO_PHILIA
+        
+        if (
+            (self.user1[1] == self.user2[2] and self.user1[2] == self.user2[1]) and 
+            (self.user1[0] == self.user2[3] and self.user1[3] == self.user2[0])
+        ):
+            return self.RELATIONSHIP_FULL_AGAPE
+
+        if self.user1[0] == self.user2[0] and self.user1[1] == self.user2[2]:
+            return self.RELATIONSHIP_AGAPE
+            
+        if (
+            (self.user1[0] == self.user2[2] or self.user1[2] == self.user2[0]) and
+            (self.user1[1] == self.user2[3] or self.user1[3] == self.user2[1])
+        ):
+            return self.RELATIONSHIP_FULL_EROS
+
+        if self.user1[0] == self.user2[2] or self.user1[2] == self.user2[0]:
+            return self.RELATIONSHIP_EROS
+
+        if self.user1[1] == self.user2[3] or self.user1[3] == self.user2[1]:
+            return self.RELATIONSHIP_EROS_VARIANT
+
+        return "Unknown Relationship"
+
+
+    def get_comfort_score(self, relationship_type):
+        """Return the comfort score for the given relationship type."""
+        return self.COMFORT_SCORES.get(relationship_type, 0)  # Default to 0 if relationship type is not found
+    
+    
+    def get_all_types(self):
+
+        # use typology aspects to get all types
+        aspects = self.typology.get_aspects()
+        # combinations of 4 aspects without repetition
+        return [", ".join([str(aspect) for aspect in perm]) for perm in permutations(aspects, 4)]
+    
+    def shorten_type(self, types):
+        """Shorten the type name to the first letter of each aspect."""
+        if isinstance(types, str):
+            # If types is a string, convert it to a list with one element
+            types = [types]
+        # Shorten each type name to the first letter of each aspect
+        return ["".join([aspect[0] for aspect in type_name.split(" ")]) for type_name in types]
+
+# create an instance of the RelationshipCalculator class
+calculator = RelationshipCalculator([], [], TypologyTemporistics())
+
+# get all types
+all_types = calculator.get_all_types()
+print(all_types)
+
+# shorten the type names
+shortened_types = calculator.shorten_type(all_types)
+
+# print the shortened type names
+print(shortened_types)
--- typology_psychosophia.py
+++ typology_psychosophia.py
@@ -1,4 +1,4 @@
-from typology import Typology
+from relationship_calculator import Typology
 
 class TypologyPsychosophia(Typology):
     def __init__(self):
@@ -6,3 +6,29 @@ class TypologyPsychosophia(Typology):
         Initializes a new instance of the TypologyPsychosophia class.
         """
         super().__init__(["Emotion", "Logic", "Will", "Power"])
+    
+    def get_all_quadras(self):
+        """
+        Returns all quadras of psychosophy with their descriptions.
+        
+        Each quadra consists of four types that share common values and characteristics.
+        """
+        quadras = {
+            'First': {
+                'types': ['Andersen (ELWF)', 'Ghazali (EWLV)', 'Goethe (PWLE)', 'Aristippus (PLWE)'],
+                'description': 'The first psychosophic quadra is characterized by processional Will and Logic, as well as resultative Physics and Emotion. Its representatives value arguments and heated discussions. Proving one\'s rightness is important for the types of this quadra, because their self-esteem depends significantly on it. These types are usually unpretentious in everyday life and entertainment, which is why their personal relationships are the strongest compared to other quadras. At the same time, they are ambitious, like to discuss their goals and decisions.'
+            },
+            'Second': {
+                'types': ['Pushkin (EPWL)', 'Pasternak (EWPL)', 'Lao Tzu (LPWE)', 'Plato (LWPE)'],
+                'description': 'The second psychosophic quadra is characterized by processional Will and Physics, as well as resultative Emotion and Logic. It includes such types as Pushkin (EPWL), Pasternak (EWPL), Lao Tzu (LPWE) and Plato (LWPE). This quadra can be called "the most silent" of all, since those aspects that are primarily manifested through speech (Emotion with Logic) are in positions of functions with the property "result". For these types, the collective and interaction with it are very important. They like to cooperate and compete with other people, gather a team to achieve goals and develop joint strategies. Also for them, the primary issue is the beauty and aesthetics of the material world. They have a desire to decorate and bring to perfection their place of residence or work. In people, they value grooming more than just a beautiful appearance. For them it is important that a person constantly improves, making efforts for this, and only such people, in their understanding, deserve respect. The strong sides of the types of the second quadra are: the ability to achieve long-term goals, good taste, care for their health, tolerance for stormy emotional manifestations, constant work on themselves. Their weak sides are unwillingness to question established dogmas, lack of charisma and low level of empathy.'
+            },
+            'Third': {
+                'types': ['Rousseau (ELFW)', 'Bukharin (EWFL)', 'Napoleon (PFLW)', 'Lenin (PLWF)'],
+                'description': 'The third psychosophic quadra is characterized by processional Physics and Logic, as well as resultative Will and Emotion. It includes such types as Rousseau (ELFW), Bukharin (EWFL), Napoleon (PFLW) and Lenin (PLWF). Its representatives are "people of the Renaissance era" who make the world beautiful and spiritualized. These types are active and mobile, constantly improving their body, love walks and nature. This helps them get a lot of pleasure from life, maintain good health for many years. And an inexhaustible interest in knowing the world makes them study a lot, think, discuss on all sorts of philosophical topics. Types of the third quadra show themselves well in intellectual activity, as well as in those types of art that require excellent mastery of technique - handicrafts, drawing, classical dances and music. Their strong sides are freethinking, naturalness, subtle understanding of aesthetics. Their weak sides are uncompromisingness and fussiness, a tendency to believe in beautifully designed but unconfirmed concepts.' 
+            }
+            
+        }
+        
+        
+        return quadras
+
