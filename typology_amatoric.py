7f897fac5f3f0f4d224afdb3d298488909afa1c0
Oleh Protsenko <designofhumanity@gmail.com>
2023-09-09 09:29:15 +0300

    working

--- typology_amatoric.py
+++ typology_amatoric.py
@@ -1,5 +1,18 @@
-from relationship_calculator import Typology
+from typology import Typology
+from itertools import permutations
 class TypologyAmatoric(Typology):
     def __init__(self):
       super().__init__(["Love", "Passion", "Friendship", "Romance"])
+    
+    # get aspects
+    def get_aspects(self):
+        return self.aspects
+    
+    def get_all_types(self):
+        # Assuming temporistics involves combinations of its aspects just like the socionics example
+        return [", ".join([str(aspect) for aspect in perm]) for perm in permutations(self.aspects, 4)]
 
+    def shorten_type(self, types):
+        if isinstance(types, str):
+            types = [types]
+        return ["".join([aspect[0] for aspect in type_name.split(", ")]) for type_name in types]
