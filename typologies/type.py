8f8c2a22055777bd06a7b9b0b0095accc907fdc5
Oleh Protsenko <designofhumanity@gmail.com>
2023-09-10 11:17:47 +0300

    changes

--- typologies/type.py
+++ typologies/type.py
@@ -1,45 +1,38 @@
 from abc import ABC
+from typing import Dict
 
 class Type(ABC):
 
-    def __init__(self, full_name: str, pseudonym: str, quadra: str, short_description: str, full_description: str):
+    def __init__(self, full_name: str, pseudonym: str, aspect: str, descriptions: Dict[str, Dict[str, str]]):
         self._full_name = full_name
         self._pseudonym = pseudonym
         self._short_name = self._generate_short_name()
-        self._quadra = quadra
-        self._short_description = short_description
-        self._full_description = full_description
+        self._aspect = aspect
+        self._short_description = self._generate_description(descriptions, 'short')
+        self._full_description = self._generate_description(descriptions, 'full')
 
     @property
     def full_name(self) -> str:
-        """Get the full name of the type."""
         return self._full_name
 
     @property
     def short_name(self) -> str:
-        """Get the short name of the type."""
         return self._short_name
 
     @property
     def pseudonym(self) -> str:
-        """Get the pseudonym of the type."""
         return self._pseudonym
 
     @property
-    def quadra(self) -> str:
-        """Get the quadra of the type."""
-        return self._quadra
-
-    @property
     def short_description(self) -> str:
-        """Get the short description of the type."""
         return self._short_description
 
     @property
     def full_description(self) -> str:
-        """Get the full description of the type."""
         return self._full_description
 
     def _generate_short_name(self) -> str:
-        """Generate the short name based on the full name."""
         return "".join(word[0] for word in self._full_name.split())
+
+    def _generate_description(self, descriptions: Dict[str, Dict[str, str]], desc_type: str) -> str:
+        return descriptions.get(self._aspect, {}).get(desc_type, '')
