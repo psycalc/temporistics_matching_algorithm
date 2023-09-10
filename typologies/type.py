ed90a8038cc8c90047da0c37213418659d19f5f0
Oleh Protsenko <designofhumanity@gmail.com>
2023-09-10 11:14:21 +0300

    changes

--- typologies/type.py
+++ typologies/type.py
@@ -2,10 +2,13 @@ from abc import ABC
 
 class Type(ABC):
 
-    def __init__(self, full_name: str, pseudonym: str):
+    def __init__(self, full_name: str, pseudonym: str, quadra: str, short_description: str, full_description: str):
         self._full_name = full_name
         self._pseudonym = pseudonym
         self._short_name = self._generate_short_name()
+        self._quadra = quadra
+        self._short_description = short_description
+        self._full_description = full_description
 
     @property
     def full_name(self) -> str:
@@ -21,6 +24,21 @@ class Type(ABC):
     def pseudonym(self) -> str:
         """Get the pseudonym of the type."""
         return self._pseudonym
+
+    @property
+    def quadra(self) -> str:
+        """Get the quadra of the type."""
+        return self._quadra
+
+    @property
+    def short_description(self) -> str:
+        """Get the short description of the type."""
+        return self._short_description
+
+    @property
+    def full_description(self) -> str:
+        """Get the full description of the type."""
+        return self._full_description
 
     def _generate_short_name(self) -> str:
         """Generate the short name based on the full name."""
